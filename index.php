a04ff71f4c979e44a9c4b2c8d2e2faa7c6de81b7
Valery Chen <cpan3@stuy.edu>
2018-03-04 13:49:01 -0500

    added utf coding

--- index.php
+++ index.php
@@ -1,21 +1,25 @@
 <!DOCTYPE html>
 <html>
-<head><title>PHP DEMO</title></head>
+  <head>
+    <title>PHP DEMO</title>
+    <meta charset="UTF-8">
+  </head>
 
-<body>
-<?php
-echo "hello world!";
-
-/*
-$option = (int)$_GET["drop_down"]
-if ( option == 1){
-   echo "variables!";
-}else if(option == 2){
-   echo "operators?";
-}else{
-   echo "something";
-}
-?>
-<a href="index.html">Back</a>
-</body>
+  <body>
+    <?php
+      echo "hello world!";
+      
+      /*
+      $option = (int)$_GET["drop_down"]
+      if ( option == 1){
+      echo "variables!";
+      }else if(option == 2){
+      echo "operators?";
+      }else{
+      echo "something";
+      }
+      */
+      ?>
+    <a href="index.html">Back</a>
+  </body>
 </html>
